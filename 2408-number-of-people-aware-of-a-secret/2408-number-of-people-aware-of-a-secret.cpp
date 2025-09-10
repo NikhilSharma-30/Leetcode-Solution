a3cf95fd7d23511db704f4a4b9450777a61462c9
Nikhil Sharma <114860069+NikhilSharma-30@users.noreply.github.com>
2025-09-11 01:41:53 +0530

    Time: 23 ms (10.85%), Space: 9.3 MB (86.56%) - LeetHub

---
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    vector<int>t;
+    int solve(int day,int delay,int forget){
+        if(day==1) return 1;
+        if(t[day]!=-1) return t[day];
+        int result=0;
+
+        for(int prev=day-forget+1;prev<=(day-delay);prev++){
+            if(prev>0){
+                result=(result+solve(prev,delay,forget))%mod;
+
+            }
+        }
+        return t[day]=result;
+    }
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        int total=0;
+        t.assign(n+1,-1);
+        for(int day=n-forget+1;day<=n;day++){
+            if(day>0){
+                total=(total+solve(day,delay,forget))%mod;
+            }
+        }
+        return total;
+        
+    }
+};
