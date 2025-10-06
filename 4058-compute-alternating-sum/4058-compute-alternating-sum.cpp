ea1821a8e0c979f86108cf99dce73ae71b8dbc55
Nikhil Sharma <114860069+NikhilSharma-30@users.noreply.github.com>
2025-10-07 00:16:27 +0530

    Time: 3 ms (12.5%), Space: 32.6 MB (13.82%) - LeetHub

--- 4058-compute-alternating-sum/4058-compute-alternating-sum.cpp
+++ 4058-compute-alternating-sum/4058-compute-alternating-sum.cpp
@@ -3,15 +3,15 @@ public:
     int alternatingSum(vector<int>& nums) {
         int n=nums.size();
         if(n==2) return (nums[0]-nums[1]);
-        int s0=0,s1=0;
+        int s=0;
         for(int i=0;i<n;i++){
             if(i%2==0){
-                s0+=nums[i];
+                s+=nums[i];
             }
             else{
-                s1+=nums[i];
+                s-=nums[i];
             }
         }
-        return s0-s1;
+        return s;
     }
 };
