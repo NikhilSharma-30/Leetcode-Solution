cdabb81fc90d9220cfeb8ca11a3141ca45b09460
Nikhil Sharma <114860069+NikhilSharma-30@users.noreply.github.com>
2025-10-19 10:49:22 +0530

    Time: 6 ms (0%), Space: 25.1 MB (23.08%) - LeetHub

---
+++ 4080-smallest-missing-multiple-of-k/4080-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        int n=nums.size(),x=1;
+        set<int>st;
+        sort(nums.begin(),nums.end());
+        int ma=*max_element(nums.begin(),nums.end());
+        for(auto &i:nums){
+            st.insert(i);
+        }
+        while((k*x)<=ma){
+            if(st.find(k*x)==st.end()){
+                return k*x;
+            }
+            x++;
+        }
+        return (k*x);
+        
+    }
+};
