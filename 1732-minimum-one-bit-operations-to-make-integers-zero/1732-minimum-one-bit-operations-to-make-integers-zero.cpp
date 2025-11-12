b072ef3e0435a773f167f787b56b830439a610d6
Nikhil Sharma <114860069+NikhilSharma-30@users.noreply.github.com>
2025-11-13 02:25:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (13.73%) - LeetHub

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/1732-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int mask=n;
+        while(mask>0){
+            mask>>=1;
+            n^=mask;
+        }
+        return n;
+        
+    }
+};
