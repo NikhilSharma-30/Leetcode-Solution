6a8be7d06991f4f07c9de85e1c5b78ee5023beee
Nikhil Sharma <114860069+NikhilSharma-30@users.noreply.github.com>
2025-12-08 10:55:09 +0530

    Time: 12 ms (42.48%), Space: 7.8 MB (84.51%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -4,9 +4,11 @@ public:
         int c=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                for(int k=1;k<=n;k++){
-                    if((i*i+j*j)==(k*k)) c++;
-                }
+                int p=i*i+j*j;
+                float sq=sqrt(p);
+                int x=floor(sq);
+                float diff=sq-x;
+                if(diff==0 && x<=n) c++;
             }
         }
         return c;
