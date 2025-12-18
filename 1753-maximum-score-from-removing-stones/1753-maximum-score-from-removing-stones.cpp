8589ad3ae0422b2e9950737c8336b697b9e7bbcb
Nikhil Sharma <114860069+NikhilSharma-30@users.noreply.github.com>
2025-12-18 06:11:39 +0530

    Time: 54 ms (46.77%), Space: 7.9 MB (47.03%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int maximumScore(int a, int b, int c) {
-        int ans=1;
-        vector<int>v;
-        v.push_back(a);
-        v.push_back(b);
-        v.push_back(c);
-        sort(v.begin(),v.end());
-        int p=v[1]+v[0];
-        if(p<v[2]) return p;
-        int cal=((v[0]+v[1])-(v[2]-1))/2;
-        int t=(v[2]-1);
-        if((v[0]+v[1]-2*cal)==(v[2])) cal++;
-        return t+cal;
+        int ans=0;
+        priority_queue<int>pq;
+        pq.push(a);
+        pq.push(b);
+        pq.push(c);
+        int co=0;
+        while(true){
+            int x1=pq.top()-1;
+            pq.pop();
+            int x2=pq.top()-1;
+            pq.pop();
+            if(x1==0) co++;
+            if(x2==0) co++;
+            if(co>=2) break;
+            pq.push(x1);
+            pq.push(x2);
+            ans++;
+        }
+        return (ans+1);
+        
     }
 };
