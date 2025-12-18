f7c611234c61bb076467cc1e4131fba63799cf8c
Nikhil Sharma <114860069+NikhilSharma-30@users.noreply.github.com>
2025-12-18 06:31:29 +0530

    Time: 0 ms (100%), Space: 8.5 MB (11.75%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -13,17 +13,11 @@ public:
             st.push(word[i]);
         }
         if(id==-1) return word;
-        string ans="";
-        while(!st.empty()){
-            ans+=(st.top());
+        id=id+1;
+        for(int i=0;i<id;i++){
+            word[i]=st.top();
             st.pop();
         }
-        int x=id+1;
-        if(x<n){
-            for(int i=x;i<n;i++){
-                ans+=word[i];
-            }
-        }
-        return ans;
+        return word;
     }
 };
