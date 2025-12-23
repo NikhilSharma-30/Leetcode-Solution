ac57793f157af901d8f041341d0f20e1d8267dc3
Nikhil Sharma <114860069+NikhilSharma-30@users.noreply.github.com>
2025-12-24 02:04:41 +0530

    Time: 0 ms (100%), Space: 12.4 MB (38.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0,j=0;
-        map<int,int>mp;
-        for(auto &x:nums1){
-            if(j<m){
-                mp[x]++;
-                j++;
+        int i=m-1,j=n-1;
+        int k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+                k--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                j--;
+                k--;
             }
         }
-        for(auto &x:nums2){
-            mp[x]++;
+        while(j>=0){
+            nums1[k]=nums2[j];
+            k--;
+            j--;
         }
-        for(auto &x:mp){
-            int val=x.first;
-            for(int j=0;j<x.second;j++){
-                if(i<(m+n)){
-                    nums1[i]=val;
-                }
-                i++;
 
-            }
-        }
+        
 
     }
 };
