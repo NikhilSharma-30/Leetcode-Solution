ef220f811aeb173c5a9fb2620ab87e8abf317bf6
Nikhil Sharma <114860069+NikhilSharma-30@users.noreply.github.com>
2025-12-30 13:15:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        int sq=sqrt(num);
-        if((sq*sq)==num) return true;
+        int l=1,r=num;
+        if(num==1) return true;
+        while(l<r){
+            int m=l+(r-l)/2;
+            long long s=1LL*m*m;
+            if(s==num){
+                return true;
+            }
+            else if(s<num){
+                l=m+1;
+            }
+            else{
+                r=m-1;
+            }
+        }
         return false;
         
     }
