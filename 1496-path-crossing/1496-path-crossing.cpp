4c7bf50dff9fab4ab805cc5f511f1890a0188340
Nikhil Sharma <114860069+NikhilSharma-30@users.noreply.github.com>
2026-01-05 09:44:52 +0530

    Time: 0 ms (100%), Space: 9.8 MB (39.89%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        set<vector<int>>st;
+        int x=0,y=0;
+        st.insert({0,0});
+        for(auto &c:path){
+            if(c=='N'){
+                y++;
+            }
+            else if(c=='S'){
+                y--;
+            }
+            else if(c=='E'){
+                x++;
+            }
+            else{
+                x--;
+            }
+            if(st.find({x,y})!=st.end()){
+                return true;
+            }
+            st.insert({x,y});
+        }
+        return false;
+        
+    }
+};
