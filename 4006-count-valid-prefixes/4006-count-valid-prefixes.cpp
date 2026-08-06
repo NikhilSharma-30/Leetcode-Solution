6aac06b42bdffec24f2a20888c417b16b595d48c
Nikhil Sharma <114860069+NikhilSharma-30@users.noreply.github.com>
2026-08-07 00:20:57 +0530

    Time: 0 ms (100%), Space: 9.4 MB (85.04%) - LeetHub

---
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int n=s.size();
+        int ans=0;
+        int c0=0,c1=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0'){
+                c0++;
+            }
+            else{
+                c1++;
+            }
+            if(abs(c0-c1)<=1){
+                ans++;
+            }
+        }
+        return ans;
+        
+    }
+};
